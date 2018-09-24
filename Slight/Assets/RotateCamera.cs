0a069ae249ce111f3129bf7b75b63eef8ed1565b
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-09-24 12:26:32 -0700

    Add mouse look

--- Slight/Assets/RotateCamera.cs
+++ Slight/Assets/RotateCamera.cs
@@ -2,15 +2,29 @@
 
 public class RotateCamera : MonoBehaviour {
 
-    public Vector3 camRotation;
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 1.1f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // Camera Control
+        
+
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(-mouseLook.y, mouseLook.x, 0f);
+    }
 }
