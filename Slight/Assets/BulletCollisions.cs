bfc4e83fec4f56dbe329a7bae13f53144cb0d052
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-10-12 12:18:25 -0700

    Added bullet deletion on collision

---
+++ Slight/Assets/BulletCollisions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BulletCollisions : MonoBehaviour {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.isTrigger)
+        {
+            // Do stuff
+            Destroy(this.gameObject);
+        }
+    }
+}
