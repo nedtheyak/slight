36fda398f3fc83e2b94ed16ee232e68884375062
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-10-16 12:18:12 -0700

    Added enemy spawners

---
+++ Slight/Assets/EnemySpawnerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+
+public class EnemySpawnerController : MonoBehaviour {
+
+    private List<GameObject> myEnemies = new List<GameObject> { };
+    public float timer;
+    public GameObject enemyPrefab;
+
+	void Start () {
+        timer = Time.deltaTime;
+        enemyPrefab = Resources.Load("prefabs/Enemy") as GameObject;
+        myEnemies.Add((GameObject)Instantiate(
+            enemyPrefab,
+            this.gameObject.transform.position,
+            Quaternion.Euler(new Vector3(0f, 0f, 0f))));
+    }
+	
+	void FixedUpdate () {
+        timer += Time.deltaTime;
+        if (timer > 5f)
+        {
+            
+            myEnemies.Add((GameObject)Instantiate(
+                enemyPrefab,
+                this.gameObject.transform.position,
+                Quaternion.Euler(new Vector3(0f, 0f, 0f))));
+            timer = 0f;
+        }
+    }
+}
