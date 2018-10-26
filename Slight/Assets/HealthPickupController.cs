546341d7e342a91086d717ede736fdbb8f070bec
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-10-26 11:23:29 -0700

    Finished implementing health pickups

---
+++ Slight/Assets/HealthPickupController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickupController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.GetComponent<Rigidbody>() && collider.name == "Player(Clone)")
+        {
+            GameObject.Find("Player(Clone)").GetComponent<PlayerHealth>().UpdateHealth(25, true);
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
