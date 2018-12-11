045ab31c4e38450bd2deeaf6ddf1567043fc1a87
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-12-11 11:54:43 -0800

    Added explanatory comments for EnemySpawnerHandlerController script

--- Slight/Assets/EnemySpawnerHandlerController.cs
+++ Slight/Assets/EnemySpawnerHandlerController.cs
@@ -1,29 +1,49 @@
-﻿using System.Collections;
+﻿/// This script handles the handler for all enemy spawners
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawnerHandlerController : MonoBehaviour {
 
+    // Used for keeping track of all enemies in the world
     public List<GameObject> myEnemies = new List<GameObject> { };
+
+    // Used for tracking when to spawn
+    // Whether or not to continue spawning
     public bool spawnMore = true;
+    
+    // Used to *force* stop of spawning
     public bool stopSpawning = false;
+    
+    // Used to track the previous enemy count (for comparison against new)
     public float oldCount;
+
+    // This is used for adding points on enemy removal
     public PointsHandlerController pointsHandlerScript;
 
+
+    // This is for removing enemies
     public void RemoveEnemy(GameObject givenEnemy)
     {
+        // Update enemy count
         oldCount = myEnemies.Count;
+        // Remove the enemy from the list
         myEnemies.Remove(givenEnemy);
+        // Remove the enemy from the scene
         Destroy(givenEnemy);
+        // Add points to the counter if the enemy count has diminished
         pointsHandlerScript.AddPoints((int)(oldCount - myEnemies.Count));
     }
 
-    // Use this for initialization
+    // Initialization
     void Start () {
+        // Get the pointsHandler's script
         pointsHandlerScript = GameObject.Find("PointsHandler").GetComponent<PointsHandlerController>();
     }
 	
-	// Update is called once per frame
+    // Every frame, update whether or not to spawn more enemies
 	void Update () {
         if (myEnemies.Count != oldCount && !stopSpawning)
         {
