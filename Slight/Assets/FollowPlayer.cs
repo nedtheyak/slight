eb1f15b96331e9cf20544bc85bbd4984d99b8054
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-12-11 12:14:53 -0800

    Added/modified explanatory comments for FollowPlayer script

--- Slight/Assets/FollowPlayer.cs
+++ Slight/Assets/FollowPlayer.cs
@@ -1,18 +1,26 @@
-﻿using UnityEngine;
+﻿/// This script handles the movement of the camera, keeping it inside of the player (with an offset)
+
+
+using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour {
+
 
     public GameObject player;
     public Vector3 offset;
 
-	// Use this for initialization
+	// Initialization
 	void Start () {
+        // Get player
         player = GameObject.Find("Player(Clone)");
+
+        // Set offset
         offset = new Vector3(0, 1, 0);
     }
 	
-	// Update is called once per frame
+	// Move camera into player (with offset) every frame
 	void Update () {
+        // Set camera position
         transform.position = player.transform.position + offset;
 	}
 }
