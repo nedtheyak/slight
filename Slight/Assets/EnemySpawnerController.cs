a120c8ac6d4fc70c77580e2c0be01b80d2c7fbb2
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-12-11 11:59:00 -0800

    Added explanatory comments for EnemySpawnerController script

--- Slight/Assets/EnemySpawnerController.cs
+++ Slight/Assets/EnemySpawnerController.cs
@@ -1,18 +1,35 @@
-﻿using UnityEngine;
+﻿/// This script handles enemy spawners
+
+
+using UnityEngine;
 using UnityEngine.UI;
 using System;
 using System.Collections.Generic;
+
+
+
 
 public class EnemySpawnerController : MonoBehaviour {
     
     public float timer;
     public GameObject enemyPrefab;
+
+    // EnemySpawnerHandler script for adding enemies to the comprehensive list of enemies
     public EnemySpawnerHandlerController handlerScript;
+
+    // Create randomizer
     private System.Random rnd = new System.Random();
 
+
+
 	void Start () {
+        // Add an initial timer offset so spawners don't all spawn simultaneously
         timer = -rnd.Next(0, 5);
+
+        // Load enemy prefab
         enemyPrefab = Resources.Load("prefabs/Enemy") as GameObject;
+
+        // Get EnemySpawnerHandler script
         handlerScript = this.gameObject.GetComponentInParent<EnemySpawnerHandlerController>();
     }
 	
@@ -22,10 +39,13 @@ public class EnemySpawnerController : MonoBehaviour {
             timer += Time.deltaTime;
             if (timer > 5f)
             {
+                // Spawn enemy and add it to the comprehensive list of enemies
                 handlerScript.myEnemies.Add((GameObject)Instantiate(
                     enemyPrefab,
                     this.gameObject.transform.position,
                     Quaternion.Euler(new Vector3(0f, 0f, 0f))));
+
+                // Reset the timer
                 timer = 0f;
             }
         }
