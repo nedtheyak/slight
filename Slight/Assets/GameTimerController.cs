51f0737b5cde6ed55310167fc0f36da29385dfcf
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-12-11 12:22:40 -0800

    Added/modified explanatory comments for GameTimerController script

--- Slight/Assets/GameTimerController.cs
+++ Slight/Assets/GameTimerController.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
+﻿/// This script handles the global game timer
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
+
 public class GameTimerController : MonoBehaviour {
 
+    // Variables
+    // The timer float itself
     public float timer;
+    
+    // The timer textbox's text component
     public Text timerCounter;
+
+    // The pointHandler and its text component and script
     public GameObject pointHandler;
     public Text pointCounterText;
     public PointsHandlerController pointsHandlerScript;
+
+    // EnemySpawnerHandler script, so spawning can be stopped when the timer is done
     public EnemySpawnerHandlerController enemySpawnerHandlerScript;
 
-    // Use this for initialization
+    // Initialization
     void Start () {
         timer = 100f;
         timerCounter = GameObject.Find("TimerCounter").GetComponent<Text>();
@@ -22,23 +36,35 @@ public class GameTimerController : MonoBehaviour {
         enemySpawnerHandlerScript = GameObject.Find("EnemySpawnerHandler").GetComponent<EnemySpawnerHandlerController>();
     }
 	
-	// Update is called once per frame
+	// Constantly reduce timer until it is done, then stop spawning and display points
 	void Update () {
 		if (timer > 0f)
         {
+            // Reduce timer
             timer -= Time.deltaTime;
+            
+            // Update timer display
             timerCounter.text = ((int)timer).ToString();
         }
         else
         {
+            // Disallow editing points
             pointsHandlerScript.pointsEditable = false;
+
+            // Stop enemy spawning
             enemySpawnerHandlerScript.spawnMore = false;
             enemySpawnerHandlerScript.stopSpawning = true;
+
+            // Remove crosshair
             Destroy(GameObject.Find("Crosshair"));
+
+            // Move PointCounter into center
             pointCounterText.rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
             pointCounterText.rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
             pointCounterText.rectTransform.pivot = new Vector2(0.5f, 0.5f);
             pointCounterText.alignment = TextAnchor.MiddleCenter;
+
+            // Enlarge PointCounter text
             pointCounterText.fontSize = 45;
         }
 	}
