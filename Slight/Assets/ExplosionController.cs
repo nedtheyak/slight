91e1c481e4f8b6183ce65580cd063d3f8f6fa959
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-12-11 12:12:29 -0800

    Added/modified explanatory comments for ExplosionController script

--- Slight/Assets/ExplosionController.cs
+++ Slight/Assets/ExplosionController.cs
@@ -1,30 +1,41 @@
-﻿using System.Collections;
+﻿/// This script handles explosions
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class ExplosionController : MonoBehaviour {
 
+    // Variables
+    // Maximum explosion radius
     public float maxScale = 5f;
+
+    // Initialize timer
     public float timer = 0.00000000000000000001f;
+    
+    // Maximum time
     public float timerMax = 1f;
 
-	// Use this for initialization
-	void Start () {
-        
-    }
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.name == "Enemy(Clone)")
         {
+            // Destroy enemies on contact
             Destroy(collider.gameObject);
         }
     }
 
-    // FixedUpdate is updated based on time, in sync with the physics engine
+    // Set scale and update timer, destroy explosion when timer hits zero
     void FixedUpdate () {
+        // Set the scale to a percentage of the max
         transform.localScale = new Vector3(timer * maxScale, timer * maxScale, timer * maxScale);
 
+        // Update timer
         if (timerMax == 1f)
         {
             timer += Time.deltaTime;
@@ -37,6 +48,7 @@ public class ExplosionController : MonoBehaviour {
             timer -= Time.deltaTime * 1.5f;
         }
 
+        // Destroy explosion when timer hits zero
         if (timer <= 0f)
         {
             Destroy(this.gameObject);
