7a041441f71f6829098c51e7b01b8e3eb7779a2b
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-12-12 11:32:04 -0800

    Added/modified explanatory comments for PointsHandlerController script

--- Slight/Assets/PointsHandlerController.cs
+++ Slight/Assets/PointsHandlerController.cs
@@ -1,33 +1,38 @@
-﻿using System.Collections;
+﻿/// This script handles the PointsHandler interactions
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
 public class PointsHandlerController : MonoBehaviour {
 
+    // Variables
     public int totalPoints;
     public Text pointCounterText;
     public bool pointsEditable;
 
+
+    // Function for adding points
     public void AddPoints(int pointsToAdd) {
         if (pointsEditable)
         {
+            // Add the points
             totalPoints += pointsToAdd;
+
+            // Update the point counter
             pointCounterText.text = "Points: " + totalPoints.ToString();
         }
     }
-
-
-	// Use this for initialization
+    
+	// Initialization
 	void Start () {
         pointsEditable = true;
         totalPoints = 0;
         pointCounterText = GameObject.Find("PointCounter").GetComponent<Text>();
         pointCounterText.text = "Points: " + totalPoints.ToString();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
