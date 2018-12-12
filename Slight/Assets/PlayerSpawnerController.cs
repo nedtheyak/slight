120aff5f10088a19968aecce87bd8ff64074763e
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-12-12 11:25:53 -0800

    Added/modified explanatory comments for PlayerSpawnerController script

--- Slight/Assets/PlayerSpawnerController.cs
+++ Slight/Assets/PlayerSpawnerController.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
+﻿/// This script handles the player spawner
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerSpawnerController : MonoBehaviour {
 
+    // Variables
     public bool spawn;
     public float timer;
     public bool newPlayer;
@@ -12,28 +18,32 @@ public class PlayerSpawnerController : MonoBehaviour {
 
 
 
-    // Use this for initialization
+    // Initialization
     void Start () {
         playerPrefab = Resources.Load("prefabs/Player") as GameObject;
         spawn = true;
         followPlayerScript = GameObject.Find("Camera").GetComponent<FollowPlayer>();
     }
 	
-	// Update is called once per frame
+
 	void Update () {
 		if (spawn)
         {
+            // Create new player
             Instantiate(
                 playerPrefab,
                 this.gameObject.transform.position,
                 Quaternion.Euler(new Vector3(0f, 0f, 0f)));
             spawn = false;
             newPlayer = true;
+
+            // Ensure camera movement gets properly reset
             followPlayerScript.player = GameObject.Find("Player(Clone)");
         }
         if (newPlayer)
         {
-            if (timer > 0.2f)
+            // Allow time for other scripts to update accordingly
+            if (timer > 0.21f)
             {
                 newPlayer = false;
                 timer = 0f;
