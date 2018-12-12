7690ef57f40ae196b3078e18851c614ca7f5a018
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-12-12 11:11:41 -0800

    Added/modified explanatory comments for HealthPickupController script

--- Slight/Assets/HealthPickupController.cs
+++ Slight/Assets/HealthPickupController.cs
@@ -1,19 +1,21 @@
-﻿using System.Collections;
+﻿/// This script handles the health pickup interactions
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthPickupController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
+    // Contact with player adds health to player and destroys self
     void OnTriggerEnter(Collider collider)
     {
         if (collider.GetComponent<Rigidbody>() && collider.name == "Player(Clone)")
         {
+            // Add health
             GameObject.Find("Player(Clone)").GetComponent<PlayerHealth>().UpdateHealth(25, true);
+
+            // Destroy self
             Destroy(transform.parent.gameObject);
         }
     }
