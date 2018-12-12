99b7f27ed1587a802b688f4679c55f4daab61197
Ned the Yak <nedtheyak@magmablock.x10host.com>
2018-12-12 11:18:05 -0800

    Added/modified explanatory comments for RotateCamera script

--- Slight/Assets/RotateCamera.cs
+++ Slight/Assets/RotateCamera.cs
@@ -1,29 +1,36 @@
-﻿using UnityEngine;
+﻿/// This script handles the rotation of the camera using mouse inputs
+
+
+using UnityEngine;
 
 public class RotateCamera : MonoBehaviour {
 
+    // Variables
     Vector2 mouseLook;
     Vector2 smoothV;
     public float sensitivity = 5.0f;
     public float smoothing = 2f;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
 	
-	// Update is called once per frame
 	void Update () {
-        // Camera Control
+        // Camera control with mouse
         
+
+        // Get inputs
         var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
+        // Convert to scaled inputs (sensitivity & smoothing)
         md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+        // Smooth inputs
         smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
         smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
         mouseLook += smoothV;
+
+        // Lock y axis from going further than straight up or down
         mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
 
+        // Apply rotations
         transform.localRotation = Quaternion.Euler(-mouseLook.y, mouseLook.x, 0f);
     }
 }
